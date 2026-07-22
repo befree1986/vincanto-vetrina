9222da234f1607944c86ce9fa424910411f18ccb
befree1986 <g.marino787@gmail.com>
2026-07-22 12:12:04 +0200

    nuovo pannello admin

--- src/pages/AdminPanelPro.tsx
+++ src/pages/AdminPanelPro.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './AdminPanelPro.css';
 import AdminLayout from '../components/admin/AdminLayout';
 import '../styles/AdminSuperAdmin.css';
